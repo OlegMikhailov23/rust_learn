0222f1391c8b99b91b42b3b3efe50de14b0445a5
Oleg Mikhaylov <dkfolkin@gmail.com>
2023-04-08 00:05:46 +0300

    cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-30d365a3ecdcab6f.d
@@ -0,0 +1,5 @@
+/Users/Dkfolkin/rust/hello_cargo/target/debug/deps/hello_cargo-30d365a3ecdcab6f.rmeta: src/main.rs
+
+/Users/Dkfolkin/rust/hello_cargo/target/debug/deps/hello_cargo-30d365a3ecdcab6f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/Dkfolkin/rust/hello_cargo/target/release/hello_cargo: /Users/Dkfolkin/rust/hello_cargo/src/main.rs
