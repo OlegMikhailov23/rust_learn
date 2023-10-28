235b2b8de0fcc50f2c2c633aebefcf9844cb2a8c
Oleg Mikhaylov <dkfolkin@gmail.com>
2023-10-28 14:26:10 +0300

    finalyze task

--- pig-code/src/main.rs
+++ pig-code/src/main.rs
@@ -16,17 +16,17 @@ fn main() {
 
     for c in word.chars() {
         if count == 1 { break; }
-        let mut target = c;
+        let target = c;
         if consonants.contains(&target) {
             let suffix = String::from("-hay");
-            let mut output = word.to_owned() + &suffix;
+            let output = word.to_owned() + &suffix;
             println!("OUTPUT {}", output);
         } else {
             let first_char = &word[0..1];
             let suffix = first_char.to_owned() + &String::from("ay");
             let divider = String::from("-");
             let sliced_word = &word[1..];
-            let mut output = sliced_word.to_owned() + &divider + &suffix;
+            let output = sliced_word.to_owned() + &divider + &suffix;
             println!("OUTPUT {output}");
         }
         count += 1;
