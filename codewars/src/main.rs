4367200009e3f1740f985b3d64fec15faea31a08
Oleg Mikhaylov <dkfolkin@gmail.com>
2023-11-29 09:55:59 +0300

    some tasks

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     grow([1,2,3,5,7,8,9].to_vec());
+    let alt_string = to_alternating_case("hello WORLD");
+    println!("alternative string: {}", alt_string);
+    let powers_of_two_res = powers_of_two(37);
+    println!("powers of two: {:?}", powers_of_two_res);
 }
 
 fn grow(nums: Vec<i32>) -> i32 {
@@ -9,4 +13,30 @@ fn grow(nums: Vec<i32>) -> i32 {
     }
     println!("{}", summ);
     return summ;
+}
+
+fn to_alternating_case(s: &str) -> String {
+  let result = s.chars().map(|c| {
+    if c.is_ascii_lowercase() {
+        c.to_ascii_uppercase()
+    } else if c.is_ascii_uppercase() {
+        c.to_ascii_lowercase()
+    } else {
+        c
+    }
+  }).collect();
+
+  result
+}
+
+fn powers_of_two(n: u8) -> Vec<u128> {
+    let mut res:Vec<u128> = Vec::new();
+    let exp: i32 = 2;
+
+    for i in 0..= n {
+        res.push(exp.pow(i.into()).try_into().unwrap());
+        println!("{}", i)
+    }
+
+    res
 }
